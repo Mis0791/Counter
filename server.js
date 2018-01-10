0ac3ddd71e8393772c36fce7f9bb342bdb325174
Michelle Sturgill <isabelsturgill@comcast.net>
2018-01-10 15:38:04 -0800

    add

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require("express");
-
 var app = express();
-
 var port = 8000;
-
 var bp = require("body-parser");
-
 var path = require("path");
-
 var session = require("express-session");
 
 app.use(bp.urlencoded());
@@ -20,9 +15,19 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.set("view.engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/count", function(req, res){
 
+    if(!req.session.count){
+        req.session.count = 0
+    }
+    res.render("count.ejs", {count: req.session.count})
+    
 });
+
+app.get("/add1", function(req, res){
+    req.session.count += 1
+    res.redirect("/count")
+})
 
 app.listen(port, function(){
     console.log("listening");
